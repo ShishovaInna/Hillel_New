c66b06270ecc397dc256ca714d680135e27183ba
Shyshova Inna <shishova.inna@gmail.com>
2026-02-02 14:47:54 +0200

    ДЗ 5.1. Ім'я змінної - виправила перевірку на введення "_"

--- 05/5_1.py
+++ 05/5_1.py
@@ -1,7 +1,7 @@
 import keyword
 str_variable = input('Введіть назву змінної: ')
 if (str_variable.isidentifier()
-    and str_variable.islower()
+    and not any(char.isupper() for char in str_variable)
     and '__' not in str_variable
     and not keyword.iskeyword(str_variable)):
     print('True')
