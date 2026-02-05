9f7cde0ef9e276f719216392e4e48c29c4f9a6c6
Shyshova Inna <shishova.inna@gmail.com>
2026-02-05 10:03:09 +0200

    додала перевірку по підкресленням

--- 05/5_1.py
+++ 05/5_1.py
@@ -1,8 +1,17 @@
 import keyword
 str_variable = input('Введіть назву змінної: ')
+
+if (str_variable.replace('_', '') == ''):
+    if (str_variable == '_'):
+      underline = True
+    else:
+      underline = False
+else:
+    underline = True
+
 if (str_variable.isidentifier()
+    and underline
     and not any(char.isupper() for char in str_variable)
-    and '__' not in str_variable
     and not keyword.iskeyword(str_variable)):
     print('True')
 else:
